af6794848f76db61b404674488706423edadb4ac
shagnzhanwei <s15838257347@126.com>
2017-08-30 11:15:03 +0800

    修改服务器地址

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '59.110.143.223',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'shop',
     // 用户名
