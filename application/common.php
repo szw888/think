756927c035111d78631a522ff8679295980a31e3
szw888 <1418712841@qq.com>
2017-09-03 12:03:16 +0800

    修改命名空间

--- application/common.php
+++ application/common.php
@@ -34,7 +34,7 @@ function doCurl($url,$type=0,$data=''){
 
 
 function send($to,$title,$body){
-    vendor('phpmailer.phpmailer');
+    vendor('phpmailer.PHPMailer');
     $mail = new PHPMailer();
     $mail->IsSMTP();
     $mail->CharSet='UTF-8'; //设置邮件的字符编码，这很重要，不然中文乱码
