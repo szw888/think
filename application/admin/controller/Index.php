60bd990d5544ef3bc7004f18c7b0d7a17d1c2b03
szw888 <1418712841@qq.com>
2017-09-03 11:53:05 +0800

    修改命名空间

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,13 +1,13 @@
 <?php
 namespace app\admin\controller;
-use phpmailer;
+use phpmailer\Email;
 use think\Controller;
 
 class Index extends Controller
 {
     public function index()
     {
-        $email = new email();
+        $email = new Email();
         $email->send('1','1','1');
 
         return $this->fetch();
