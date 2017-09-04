339bceca9ddaa33df281eeb153e41bc02a1a913e
shagnzhanwei <s15838257347@126.com>
2017-09-04 16:06:56 +0800

    修改index控制器

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,7 +6,6 @@ class Index extends Controller
 {
     public function index()
     {
-        \phpmailer\phpmailer\Email::sendmail('s15838257347@126.com','服务器测试邮件发送','发送成功');
         return $this->fetch();
     }
 }
