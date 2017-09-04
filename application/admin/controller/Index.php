9d77c417cf2135db086cbb153a5a5557df3e0839
shagnzhanwei <s15838257347@126.com>
2017-09-04 09:27:04 +0800

    邮件发送

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,15 +1,11 @@
 <?php
 namespace app\admin\controller;
-use phpmailer\email;
 use think\Controller;
 
 class Index extends Controller
 {
     public function index()
     {
-        $email = new Email();
-        $email->send('1','1','1');
-
         return $this->fetch();
     }
 }
