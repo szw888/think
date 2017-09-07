20ec91d424c97f3705bd0e1de272511857c6a022
shagnzhanwei <s15838257347@126.com>
2017-09-07 11:32:54 +0800

    修改index模块下的Index控制器

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,7 +6,6 @@ class Index
 {
     public function index()
     {
-       $res = \Map::getStaticmapBySpot('河南省郑州市金水区东风路信息学院路小铺新区26号');
-       file_put_contents('map.png',$res);
+       return 'index模块';
    }
 }
